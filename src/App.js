a7dd97f674cee595f66b9b75ca3dc1b8f36d2104
GitHub Action <action@github.com>
2020-03-09 21:56:26 +0000

    Cleaning FelipeD-FiveTalent's mess

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="Appss" alt="logo" />
         <p>
-          Edit <code>src/App.js
-            
-          </code>
+          Edit <code>src/App.js</code>
           and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
