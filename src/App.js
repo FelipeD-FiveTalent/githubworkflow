5929cf47bda2322873c8921370a157854459b998
GitHub Action <action@github.com>
2020-03-16 15:52:10 +0000

    Cleaning FelipeD-FiveTalent's lint

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
           Edit <code>src/App.js</code>
           and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" 
-        
-        target="_blank" rel="noopener noreferrer">
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn Nothing
         </a>
       </header>
